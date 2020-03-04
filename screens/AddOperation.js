79abfd96a316734d8c3565cf3a53d6451af44a99
kapilgaire <kapil@probus.io>
2020-03-04 19:13:29 +0530

    add

--- screens/AddOperation.js
+++ screens/AddOperation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import {
     View,
     Text,
@@ -10,54 +10,127 @@ import {
     Keyboard
 } from 'react-native';
 import Colors from '../constants/Colors'
-
-const AddOperation = props => {
-
-    return (
-        <TouchableWithoutFeedback onPress={() => {
-            Keyboard.dismiss();
-        }}>
-            <View style={styles.screen}>
-
-                <Text style={styles.randumNum}>0</Text>
-
-                <View style={styles.inputView} >
-                    <TextInput
-                        style={styles.inputText}
-                        placeholder="Enter Your answer"
-                        placeholderTextColor="#003f5c"
-                        keyboardType="number-pad"
-                        maxLength={9}
-                    />
-                </View>
+import { render } from 'react-dom';
 
 
+// const getRandomNum = (max, min) => {
+//     return Math.floor(Math.random() * (+max - +min) + +min);
+// };
 
-                <TouchableOpacity style={styles.startBtn}>
-                    <Text style={styles.startText}>CHECK ANS</Text>
-                </TouchableOpacity>
+// const AddOperation = props => {
+
+// const [randomNumber, setRandomNumber] = useState(0);
+
+export default class AddOperation extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            NumberHolder: 0
+        };
+    }
+
+    // generateRandomNumber
+    //     = () => {
+
+    //         const max = 10;
+    //         const min = 99;
+    //         let random =
+    //             this.setState({ NumberHolder: random });
+    //     }
+
+    showRandomNumber() {
+
+        const max = 10;
+        const min = 99;
+        for (let i = 0; i < 5; i++) {
+            setTimeout(() => {
+                // this.generateRandomNumber
+                // console.log('rand');
+                this.setState({ NumberHolder: Math.floor(Math.random() * (+max - +min) + +min) });
+
+            }, i * 1000);
+
+        }
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount()");
+
+        this.showRandomNumber();
+    }
 
 
-                <TouchableOpacity style={styles.startBtn}>
-                    <Text style={styles.startText}>START AGAIN</Text>
-                </TouchableOpacity>
-            </View>
-        </TouchableWithoutFeedback>
-    );
-};
-
-AddOperation.navigationOptions = navigationData => {
-
-    return {
+    static navigationOptions = {
         headerTitle: 'Addition',
         headerStyle: {
 
             backgroundColor: Colors.primaryColor
         },
         headerTintColor: 'white'
+        /* No more header config here! */
     };
+    render() {
+        return (
+            <TouchableWithoutFeedback onPress={() => {
+                Keyboard.dismiss();
+            }}>
+                <View style={styles.screen}>
 
-};
+                    <Text style={styles.randumNum}>{this.state.NumberHolder}</Text>
+
+                    <View style={styles.inputView} >
+                        <TextInput
+                            style={styles.inputText}
+                            placeholder="Enter Your answer"
+                            placeholderTextColor="#003f5c"
+                            keyboardType="number-pad"
+                            maxLength={9}
+                        />
+                    </View>
+
+
+
+                    <TouchableOpacity style={styles.startBtn}
+                        // onPress={() => {
+
+                        //     setRandomNumber(getRandomNum(1, 9))
+                        // }}
+
+
+                        onPress={this.generateRandomNumber}
+
+                    >
+                        <Text style={styles.startText}
+
+
+
+                        >CHECK ANS</Text>
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity style={styles.startBtn}>
+                        <Text style={styles.startText}>START AGAIN</Text>
+                    </TouchableOpacity>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+
+}
+
+// AddOperation.navigationOptions = navigationData => {
+
+//     return {
+//         headerTitle: 'Addition',
+//         headerStyle: {
+
+//             backgroundColor: Colors.primaryColor
+//         },
+//         headerTintColor: 'white'
+//     };
+
+// };
 
 const styles = StyleSheet.create({
     screen: {
@@ -93,12 +166,12 @@ const styles = StyleSheet.create({
         alignContent: "center",
         textAlign: "center",
         marginBottom: 8
-        
-        
+
+
     },
     startText: {
         color: "white"
     }
 });
 
-export default AddOperation;
+
